f6033f08bbeea576cba5634bc5ed52adcfda04c7
jinin-alj <jininaljayyousi@gmail.com>
2026-05-04 22:49:57 +0200

    version 1

--- src/acquisition/signal_source.py
+++ src/acquisition/signal_source.py
@@ -1,3 +1,11 @@
-class SignalSource:
+class IdealSignalSource(SignalSource):
+    def __init__(self, target_row_index, target_column_index):
+        self.target_row_index = target_row_index
+        self.target_column_index = target_column_index
+
     def predict_target_flash(self, group_type, group_index):
-        raise NotImplementedError
+        if group_type == "row" and group_index == self.target_row_index:
+            return True
+        if group_type == "column" and group_index == self.target_column_index:
+            return True
+        return False
